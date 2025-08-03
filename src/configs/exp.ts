bf2b4ff03f8189c51324d0f62516df3803158e88
Anshul Singh Rathore <105308981+anshul014@users.noreply.github.com>
2025-08-03 11:00:25 +0530

    add experience

--- src/configs/exp.ts
+++ src/configs/exp.ts
@@ -1,5 +1,15 @@
 export const experiences = [
   {
+    title: "Business Development Associate",
+    company: "Urban Comapny",
+    location: "Chandigarh",
+    period: "Jun 2025 - Aug 2025",
+    description:
+      "Analyzed and optimized Input Tax Credit (ITC) eligibility for clients. Assisted in GST audits, assessments, and tax authority responses. Conducted research on GST legislation to provide actionable insights.",
+    skills: ["GST", "Tax Audits", "ITC Analysis", "Regulatory Research"],
+    type: "job",
+  },
+  {
     title: "GST Taxation Executive Intern",
     company: "PwC",
     location: "Chandigarh",
