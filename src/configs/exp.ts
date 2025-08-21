6371d86e3198ab0b90ea847c90d1ee35662d7bfa
Anshul Singh Rathore <105308981+anshul014@users.noreply.github.com>
2025-08-21 12:24:35 +0530

    exp

--- src/configs/exp.ts
+++ src/configs/exp.ts
@@ -5,7 +5,13 @@ export const experiences = [
     location: "Chandigarh",
     period: "Jun 2025 - Present",
     description:
-      "Built and managed 120+ high-value partnerships, Increasing bookings by 15% in the first quarter. Optimized service delivery workflows, Reducing request loss rate by 22% through proactive follow-ups and improved lead handling.Enhanced Operational efficiency by 20% by streamlining coordination between sales, operations, and Training team. Reduced service delivery delays by 25%, improving overall customer satisfaction scores.Developed weekly KPI dashboards tracking request lost rate, Conversion rates, and operational bottlenecks,enabling faster, data-driven decisions.",
+    [
+  "Managed the fleet of 120+ partners, Resolved supply-side challenges in Chandigarh by onboarding over 20+ service partners.",
+  "Successfully tackled, RL and NR issues in Chandigarh, ensuring the city was fully operational and met quality standards.",
+  "Managed day-to-day operational tasks for service partners, including training coordination, performance monitoring, and issue resolution.",
+  "Analyzing complex data for enhancing business growth and controlling KPI like, Service Delivery, Net Promoter Score, Reduced Request Loss by 8% and Bad Customer Experience.",
+  "Provided consistent support to partners to enhance service delivery, retention, and customer experience."
+],
     skills: ["Business Development", "Sales Strategy", "Client Relationship Management", "Market Research & Analysis","Partner Onboarding"],
     type: "job",
   },
