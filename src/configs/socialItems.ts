a56ba702423a5ee86903cd698293cf19f5bb4b15
Anshul Singh Rathore <105308981+anshul014@users.noreply.github.com>
2025-09-01 16:05:00 +0530

    email

--- src/configs/socialItems.ts
+++ src/configs/socialItems.ts
@@ -1,7 +1,7 @@
 import { GitHubIcon, LinkedInIcon, MailIcon } from "@/components/SocialIcons";
 
 export const socialItems = [
-  { icon: MailIcon, href: "mailto:itzanshul.014@gmail.com", label: "mail" },
+  { icon: MailIcon, href: "mailto:itzanshul.14@gmail.com", label: "mail" },
   { icon: GitHubIcon, href: "https://github.com/anshul014", label: "github" },
   {
     icon: LinkedInIcon,
